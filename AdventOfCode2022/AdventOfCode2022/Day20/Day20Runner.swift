65d0a6b5e1f1c6de18922b0b7f84aead5216b572
Vincent Beltman <vincent.beltman@inspiro.nl>
2022-12-20 13:22:46 +0100

    Day20 improvement

--- AdventOfCode2022/AdventOfCode2022/Day20/Day20Runner.swift
+++ AdventOfCode2022/AdventOfCode2022/Day20/Day20Runner.swift
@@ -20,10 +20,15 @@ class Day20Runner: Runable
       self.right = right
       self.number = number
     }
-    func getNode(at indexFromSelf: Int) -> Node
+    func getNode(at indexFromSelf: Int, nrOfNodes: Int) -> Node
     {
-      if indexFromSelf > 0 { return right.getNode(at: indexFromSelf-1) }
-      if indexFromSelf < 0 { return left.getNode(at: indexFromSelf+1) }
+      let absIndex: Int = abs(indexFromSelf)
+      return stepGetNode(at: absIndex > (nrOfNodes/2) ? -indexFromSelf.signum() * (nrOfNodes - absIndex) : indexFromSelf)
+    }
+    func stepGetNode(at indexFromSelf: Int) -> Node
+    {
+      if indexFromSelf > 0 { return right.stepGetNode(at: indexFromSelf-1) }
+      if indexFromSelf < 0 { return left.stepGetNode(at: indexFromSelf+1) }
       return self
     }
   }
@@ -50,14 +55,14 @@ class Day20Runner: Runable
       if node.number > 0
       { // Move right
         node.right.left = node.left; node.left.right = node.right // Remove self from current position
-        let target: Node = node.getNode(at: node.number%(nodes.count-1))
+        let target: Node = node.getNode(at: node.number%(nodes.count-1), nrOfNodes: nodes.count)
         node.left = target; node.right = target.right // Place self after target
         target.right = node; node.right.left = node // Repair new neighbours
       }
       else if node.number < 0
       { // Move left
         node.right.left = node.left; node.left.right = node.right // Remove self from current position
-        let target: Node = node.getNode(at: node.number%(nodes.count-1))
+        let target: Node = node.getNode(at: node.number%(nodes.count-1), nrOfNodes: nodes.count)
         node.right = target; node.left = target.left // Place self after target
         target.left = node; node.left.right = node // Repair new neighbours
       } // Skip if number is 0
@@ -67,13 +72,13 @@ class Day20Runner: Runable
   {
     var nodes: [Node] = parseNodes(input: input)
     mix(&nodes)
-    return "\([Int](1...3).reduce(0, { $0 + nodes.first(where: { $0.number == 0 })!.getNode(at: ($1*1000) % nodes.count).number } ))"
+    return "\([Int](1...3).reduce(0, { $0 + nodes.first(where: { $0.number == 0 })!.getNode(at: ($1*1000) % nodes.count, nrOfNodes: nodes.count).number } ))"
   }
 
   func RunPart2(input: Input) -> String
   {
     var nodes: [Node] = parseNodes(input: input, multiplier: 811589153)
     for _ in 1...10 { mix(&nodes) }
-    return "\([Int](1...3).reduce(0, { $0 + nodes.first(where: { $0.number == 0 })!.getNode(at: ($1*1000) % nodes.count).number } ))"
+    return "\([Int](1...3).reduce(0, { $0 + nodes.first(where: { $0.number == 0 })!.getNode(at: ($1*1000) % nodes.count, nrOfNodes: nodes.count).number } ))"
   }
 }
